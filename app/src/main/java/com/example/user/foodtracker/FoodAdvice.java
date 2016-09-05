bd3ff53dbaf27acf0bad9696aff005e23ca12faf
Charley Lawton <user@CODECLAN055.local>
2016-09-05 15:30:32 +0100

    Advice page working

--- app/src/main/java/com/example/user/foodtracker/FoodAdvice.java
+++ app/src/main/java/com/example/user/foodtracker/FoodAdvice.java
@@ -3,8 +3,14 @@ package com.example.user.foodtracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by user on 05/09/2016.
@@ -13,8 +19,9 @@ public class FoodAdvice extends AppCompatActivity {
 
     TextView mAdviceButton;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advice);
 
